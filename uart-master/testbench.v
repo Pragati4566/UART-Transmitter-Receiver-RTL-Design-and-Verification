f197ddd63b26570b14cccfaff2dc3d27bc65b233
Pragati Chaudhary <savita845822@gmail.com>
2026-08-12 00:58:35 +0530

    updated

--- uart-master/testbench.v
+++ uart-master/testbench.v
@@ -1,18 +1,26 @@
 module testbench;
 
+    parameter DATA_WIDTH = 8;
+    parameter CLK_FREQ_HZ = 1000000;
+    parameter BAUD_RATE = 100000;
+
     reg clk;
     reg rst;
 
     reg tx_start;
-    reg [7:0] tx_data;
+    reg [DATA_WIDTH-1:0] tx_data;
 
     wire tx_line;
 
-    wire [7:0] rx_data;
+    wire [DATA_WIDTH-1:0] rx_data;
     wire rx_valid;
 
 
-    uart_tx transmitter (
+    uart_tx #(
+        .DATA_WIDTH(DATA_WIDTH),
+        .CLK_FREQ_HZ(CLK_FREQ_HZ),
+        .BAUD_RATE(BAUD_RATE)
+    ) transmitter (
         .clk(clk),
         .rst(rst),
         .tx_start(tx_start),
@@ -21,7 +29,11 @@ module testbench;
     );
 
 
-    uart_rx receiver (
+    uart_rx #(
+        .DATA_WIDTH(DATA_WIDTH),
+        .CLK_FREQ_HZ(CLK_FREQ_HZ),
+        .BAUD_RATE(BAUD_RATE)
+    ) receiver (
         .clk(clk),
         .rst(rst),
         .rx(tx_line),
@@ -38,23 +50,21 @@ module testbench;
         $dumpfile("uart_waveform.vcd");
         $dumpvars(0, testbench);
 
-        clk      = 1'b0;
-        rst      = 1'b1;
+        clk = 1'b0;
+        rst = 1'b1;
         tx_start = 1'b0;
-        tx_data  = 8'b00000000;
+        tx_data = 8'b00000000;
 
-        #10;
+        #20;
         rst = 1'b0;
 
         tx_data = 8'b10101010;
-
         tx_start = 1'b1;
 
         #10;
-
         tx_start = 1'b0;
 
-        #150;
+        #1000;
 
         $display("Transmitted Data = %b", tx_data);
         $display("Received Data    = %b", rx_data);
